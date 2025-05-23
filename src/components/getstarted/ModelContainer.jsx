8386b56bfe9f13ced6f253e0839e1cd0cbde44c2
Kishan-k2004 <k.khansali.2004@gmail.com>
2025-05-23 12:09:15 +0530

    feat: update ModelContainer text for clarity and improve user guidance

--- src/components/getstarted/ModelContainer.jsx
+++ src/components/getstarted/ModelContainer.jsx
@@ -28,10 +28,12 @@ function ModelContainer() {
   return (
     <>
         <Logo/>
+
         <div className='pl-1 pr-1 sm:pl-15 sm:pr-15'>
             <h1 className="text-3xl text-center font-medium font-Inter-Regular mt-4 text-black dark:text-white">Unlock your space for untold Stories</h1>
         </div>
         <p className='text-center font-InterLight text-xs font-extralight mt-2 mb-5  text-black dark:text-white'>Sign up for more</p>
+
         <div>
         {buttons.map((btn)=>(
             <Button key={btn.name} 
@@ -42,7 +44,7 @@ function ModelContainer() {
         ))}
         <div className="divider text-black dark:text-white before:bg-gray-300 dark:before:bg-gray-600 after:bg-gray-300 dark:after:bg-gray-600 ">or</div>
         
-        <p className='text-center font-InterLight text-xs font-extralight mt-1 mb-4  text-black dark:text-white'>Sign up for more</p>
+        <p className='text-center font-InterLight text-xs font-extralight mt-1 mb-4  text-black dark:text-white'>Already Started? Continue here</p>
         {<Button key={Login.name}
         name={Login.name} 
             lightThemeColor={Login.lightThemeColor} 
